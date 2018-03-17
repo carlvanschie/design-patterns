4a40a5e2a3e00b30ee8a8a013bdbb631c39d180b
Carl Van Schie <carl.vanschie@gmail.com>
2018-03-17 14:59:20 +0000

    Initial commit: - Visitor - State

---
+++ state/src/main/java/vanschie/state/ScanState.java
@@ -0,0 +1,12 @@
+package vanschie.state;
+
+public interface ScanState {
+
+    void start(Scan scan);
+
+    void complete(Scan scan);
+
+    void pause(Scan scan);
+
+    ScanStatus getStatus();
+}
---
+++ state/src/test/java/vanschie/state/ScanTest.java
@@ -0,0 +1,82 @@
+package vanschie.state;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static vanschie.state.ScanStatus.COMPLETED;
+import static vanschie.state.ScanStatus.PAUSED;
+import static vanschie.state.ScanStatus.RUNNING;
+
+public class ScanTest {
+
+    private final Scan scan = new Scan("TEST");
+
+
+    @Test
+    public void stateDefaultsToNew() {
+        assertThat(scan.getStatus(), is(ScanStatus.NEW));
+    }
+
+    @Test
+    public void canBeStarted() {
+        scan.start();
+        assertThat(scan.getStatus(), is(ScanStatus.RUNNING));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void cannotBeCompletedFromNew() {
+        scan.complete();
+    }
+
+    @Test
+    public void canBeCompleted() {
+        scan.start();
+        scan.complete();
+        assertThat(scan.getStatus(), is(ScanStatus.COMPLETED));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void cannotBeStartedOnceCompleted() {
+        scan.start();
+        scan.complete();
+        scan.start();
+    }
+
+    @Test
+    public void canBePausedWhenRunning() {
+        scan.start();
+        scan.pause();
+        assertThat(scan.getStatus(), is(PAUSED));
+    }
+
+    @Test
+    public void canBeStartedWhenPaused() {
+        scan.start();
+        scan.pause();
+        scan.start();
+        assertThat(scan.getStatus(), is(RUNNING));
+    }
+
+    @Test
+    public void canBeCompletedWhenPaused() {
+        scan.start();
+        scan.pause();
+        scan.complete();
+        assertThat(scan.getStatus(), is(COMPLETED));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void cannotBePausedWhenCompleted() {
+        scan.start();
+        scan.complete();
+        scan.pause();
+    }
+
+
+    @Test(expected = IllegalStateException.class)
+    public void cannotBePausedFromNew() {
+        scan.pause();
+    }
+
+}
